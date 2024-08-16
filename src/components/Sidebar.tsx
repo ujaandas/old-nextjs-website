158b51667e23d62c9bd90f8c1589da849fde1f47
Ujaan Das <ujaandas03@gmail.com>
2024-08-16 22:19:03 +0800

    Basic sidebar styling

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,7 +1,7 @@
-/* eslint-disable @next/next/no-img-element */
+import Image from "next/image";
 import aboutMe from "@/app/data/aboutMe.json";
-import { FaMapMarker, FaMapMarkerAlt } from "react-icons/fa";
-import { FaLinkedin, FaGithub, FaMap } from "react-icons/fa6";
+import { FaMapMarkerAlt } from "react-icons/fa";
+import { FaLinkedin, FaGithub } from "react-icons/fa6";
 
 export default function AboutMe() {
   const cleanUrl = (url: string) =>
@@ -10,7 +10,7 @@ export default function AboutMe() {
   return (
     <div className="flex flex-col w-full">
       <div className="flex items-center justify-start">
-        <img
+        <Image
           src="https://picsum.photos/200"
           width={200}
           height={200}
@@ -18,45 +18,55 @@ export default function AboutMe() {
         />
       </div>
       <ul className="flex flex-col">
-        <li className="flex mt-1 items-center">{aboutMe.name}</li>
-        <li className="flex items-center">@{aboutMe.username}</li>
-        <li className="flex mt-3 items-center">{aboutMe.currentRole}</li>
-        <li className="flex items-center">{aboutMe.currentCompany}</li>
-        <li className="flex mt-3 items-center">
-          <a
-            href={aboutMe.links.github}
-            target="_blank"
-            rel="noopener noreferrer"
-            className="flex mt-1 items-center"
-          >
-            <FaGithub
+        <ul className="mt-3">
+          <li className="flex items-center text-xl font-extrabold">
+            {aboutMe.name}
+          </li>
+          <li className="flex items-center text-md font-thin">
+            @{aboutMe.username} · {aboutMe.pronouns}
+          </li>
+        </ul>
+        <ul className="mt-3 text-md">
+          <li className="flex items-center">{aboutMe.currentRole}</li>
+          <li className="flex items-center">@ {aboutMe.currentCompany}</li>
+        </ul>
+        <ul className="mt-3 text-sm">
+          <li className="flex items-center">
+            <a
+              href={aboutMe.links.github}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="flex mt-1 items-center"
+            >
+              <FaGithub
+                className="text-cat-latte-text hover:text-gray-800 mr-1"
+                size={22}
+              />
+              <p>{cleanUrl(aboutMe.links.github)}</p>
+            </a>
+          </li>
+          <li className="flex mt-1 items-center">
+            <a
+              href={aboutMe.links.linkedin}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="flex mt-1 items-center"
+            >
+              <FaLinkedin
+                className="text-cat-latte-text hover:text-gray-800 mr-1"
+                size={22}
+              />
+              <p>{cleanUrl(aboutMe.links.linkedin)}</p>
+            </a>
+          </li>
+          <li className="flex mt-1 items-center">
+            <FaMapMarkerAlt
               className="text-cat-latte-text hover:text-gray-800 mr-1"
-              size={24}
+              size={22}
             />
-            <p>{cleanUrl(aboutMe.links.github)}</p>
-          </a>
-        </li>
-        <li className="flex mt-1 items-center">
-          <a
-            href={aboutMe.links.linkedin}
-            target="_blank"
-            rel="noopener noreferrer"
-            className="flex mt-1 items-center"
-          >
-            <FaLinkedin
-              className="text-cat-latte-text hover:text-gray-800 mr-1"
-              size={24}
-            />
-            <p>{cleanUrl(aboutMe.links.linkedin)}</p>
-          </a>
-        </li>
-        <li className="flex mt-1 items-center">
-          <FaMapMarkerAlt
-            className="text-cat-latte-text hover:text-gray-800 mr-1"
-            size={24}
-          />
-          <p>{aboutMe.currentLocation}</p>
-        </li>
+            <p>{aboutMe.currentLocation}</p>
+          </li>
+        </ul>
       </ul>
     </div>
   );
