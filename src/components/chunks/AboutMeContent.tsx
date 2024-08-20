011fe654ca8fd3ecb2028488ebdf7d64838b7f16
Ujaan Das <ujaandas03@gmail.com>
2024-08-20 16:15:29 +0800

    Added CV

--- src/components/chunks/AboutMeContent.tsx
+++ src/components/chunks/AboutMeContent.tsx
@@ -3,7 +3,11 @@ import PlainText from "../mdx/PlainText";
 
 async function AboutMeContent() {
   const aboutMeData = await fetchMdxByCategory("aboutMe");
-  return <PlainText content={aboutMeData[0].content} />;
+  return (
+    <div>
+      <PlainText content={aboutMeData[0].content} />
+    </div>
+  );
 }
 
 export default AboutMeContent;
