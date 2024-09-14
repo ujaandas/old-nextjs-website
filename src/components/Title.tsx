a939f75c9b9f6badb26c630a411fef97f67c6edf
Ujaan Das <ujaandas03@gmail.com>
2024-09-14 18:20:00 +0800

    Sticky headers

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -4,7 +4,7 @@ interface TitleProps {
 
 export default function Title({ title }: TitleProps) {
   return (
-    <div className="">
+    <div className="sticky top-0 z-10 bg-gradient-to-r dark:from-cat-mocha-crust from-cat-latte-base from-10% dark:to-cat-mocha-base to-cat-latte-mantle to-90% py-3 -m-2">
       <h1 className="text-3xl font-bold dark:text-cat-mocha-text text-cat-latte-text">
         {title}
       </h1>
